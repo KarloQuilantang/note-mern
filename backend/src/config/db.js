2cea417475d0e7ed6a0922d299ebe126e0407393
Karlo <quilantangkarlo@gmail.com>
2025-11-04 22:52:02 +0800

    initial commit

---
+++ backend/src/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try{
+    await mongoose.connect(process.env.MONGO_URI)
+  }catch(error) {
+    console.log("Error connecting to MONGODB", error )
+    process.exit(1) // exit with failure
+  }
+}
